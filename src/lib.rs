901a03e553c1ecefda9dd2f7e12421f47b66797c
Frank Millward <frank.millward@codethink.co.uk>
2024-04-28 17:32:26 +0100

    Zero-based indexing
    - Add n_rows and n_cols methods for getting the number of rows and columns of a matrix
    - Add get_entry and get_mut_entry for zero based indexing.

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,93 @@ impl<const M: usize, const N: usize, T: Num + Copy> Matrix<M, N, T> {
         Matrix::<M, N, T> { data }
     }
 
-    /// Extracts a slice containing the entire matrix
+    /// Extracts a slice containing the entire matrix.
     pub fn as_slice(&self) -> &[[T; N]; M] {
         &self.data
+    }
+
+    /// Return the number of rows in the matrix
+    ///
+    /// # Examples
+    ///
+    /// Get the number of rows in a 3x4 rectangular `u8` matrix
+    ///
+    /// ```
+    /// # use::num_traits::*;
+    /// use malg::Matrix;
+    /// let a = Matrix::<3,4,u8>::zero();
+    /// let n_rows = a.n_rows();
+    /// assert_eq!(n_rows, 3);
+    /// ```
+    pub fn n_rows(&self) -> usize {
+        M
+    }
+
+    /// Return the number of columns in the matrix
+    ///
+    /// # Examples
+    ///
+    /// Get the number of columns in a 3x4 rectangular `u8` matrix
+    ///
+    /// ```
+    /// # use::num_traits::*;
+    /// use malg::Matrix;
+    /// let a = Matrix::<3,4,u8>::zero();
+    /// let n_cols = a.n_cols();
+    /// assert_eq!(n_cols, 4);
+    /// ```
+    pub fn n_cols(&self) -> usize {
+        N
+    }
+
+    /// Get a borrow of a specific entry of the matrix using zero-based indexing.
+    /// If the indices lie outside of the matrix, get [`None`] instead.
+    ///
+    /// # Examples
+    ///
+    /// We can get a reference to the (1,2)th entry of the 2x3 matrix `a`
+    ///
+    /// ```
+    /// # use::num_traits::*;
+    /// use malg::Matrix;
+    /// let a = Matrix::<2,3,u8>::new([[1,2,3],[4,5,6]]);
+    /// let a12 = a.get_entry(0,1).unwrap();
+    /// assert_eq!(*a12, 2);
+    /// ```
+    ///
+    /// But trying to access the (3,2)th entry returns [`None`]
+    ///
+    /// ```
+    /// # use::num_traits::*;
+    /// # use malg::Matrix;
+    /// # let a = Matrix::<2,3,u8>::new([[1,2,3],[4,5,6]]);
+    /// let a32 = a.get_entry(2,1);
+    /// assert_eq!(a32, None);
+    /// ```
+    pub fn get_entry(&self, i: usize, j: usize) -> Option<&T> {
+        let row = self.data.get(i)?;
+        row.get(j)
+    }
+
+    /// Get a mutable borrow of a specific entry of the matrix using zero-based indexing.
+    /// If the indices lie outside of the matrix, get [`None`] instead.
+    ///
+    /// # Examples
+    ///
+    /// We can get and modify a mutable reference to the (1,2)th entry of the 2x3 matrix `a`
+    ///
+    /// ```
+    /// # use::num_traits::*;
+    /// use malg::Matrix;
+    /// let mut a = Matrix::<2,3,u8>::new([[1,2,3],[4,5,6]]);
+    /// let mut a12 = a.get_mut_entry(0,1).unwrap();
+    /// *a12 = 10;
+    /// let a12_changed = a.get_entry(0,1).unwrap();
+    /// assert_eq!(*a12_changed, 10);
+    /// ```
+    pub fn get_mut_entry(&mut self, i: usize, j: usize) -> Option<&mut T> {
+        let row = self.data.get_mut(i)?;
+        row.get_mut(j)
     }
 }
 
