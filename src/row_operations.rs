30daf9300c22f2343ea9c9365cd7f3d90f3e31db
Frank Millward <frank.millward@codethink.co.uk>
2024-06-03 17:07:09 +0100

    Implement function for getting the row echelon form for objects implementing the RowOps trait

--- src/row_operations.rs
+++ src/row_operations.rs
@@ -18,4 +18,90 @@ pub trait RowOps<Scalar: MatrixEntry + Div<Output = Scalar> + Sub<Output = Scala
     fn n_rows(&self) -> usize;
     /// Number of columns in `self`
     fn n_cols(&self) -> usize;
+    /// Calculate the row echelon form of `self` in place.
+    fn transform_to_row_echelon_form(&mut self) {
+        let mut i = 0;
+        for j in 0..self.n_cols() {
+            let mut pivot_found = false;
+            for k in i..self.n_rows() {
+                if !self.get_row(k)[j].is_zero() {
+                    if pivot_found {
+                        let leading_entry = self.get_row(k)[j];
+                        self.add_rows(k, i, Scalar::zero() - Scalar::one() * leading_entry);
+                    } else {
+                        pivot_found = true;
+                        self.swap_rows(i, k);
+                        let pivot_value = self.get_row(i)[j];
+                        self.scale_row(i, Scalar::one() / pivot_value);
+                    }
+                }
+            }
+            if pivot_found {
+                i += 1;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::error::Error;
+
+    use crate::*;
+    /// Check we can find a row-echelon form of a diagonal [`Matrix`]
+    #[test]
+    fn check_diagonal_matrix_row_echelon_form() -> Result<(), Box<dyn Error>> {
+        let mut input_matrix =
+            Matrix::<3, 3, f32>::new([[3.0, 0.0, 0.0], [0.0, 2.0, 0.0], [0.0, 0.0, 1.0]]);
+        let target_matrix =
+            Matrix::<3, 3, f32>::new([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]);
+        input_matrix.transform_to_row_echelon_form();
+        assert_eq!(input_matrix, target_matrix);
+        Ok(())
+    }
+    ///  [`Matrix`] of full rank.
+    #[test]
+    fn check_full_rank_matrix_row_echelon_form() -> Result<(), Box<dyn Error>> {
+        let mut input_matrix = Matrix::<3, 4, f32>::new([
+            [3.0, 3.0, 2.0, 1.0],
+            [1.0, 2.0, 6.0, 0.0],
+            [1.0, 0.0, 1.0, 0.0],
+        ]);
+        input_matrix.transform_to_row_echelon_form();
+        assert!(
+            input_matrix.get_entry(0, 0).expect("No value").is_one()
+                && input_matrix.get_entry(1, 0).expect("No value").is_zero()
+                && input_matrix.get_entry(1, 1).expect("No value").is_one()
+                && input_matrix.get_entry(2, 0).expect("No value").is_zero()
+                && input_matrix.get_entry(2, 1).expect("No value").is_zero()
+                && input_matrix.get_entry(2, 2).expect("No value").is_one()
+        );
+        Ok(())
+    }
+    /// Check we can find a row echelon form of a rectangular, non-square [`Matrix`] of partial rank
+    #[test]
+    fn check_partial_rank_matrix_row_echelon_form() -> Result<(), Box<dyn Error>> {
+        let mut input_matrix = Matrix::<4, 3, f32>::new([
+            [3.0, 3.0, 3.0],
+            [1.0, 2.0, 6.0],
+            [2.0, 0.0, 2.0],
+            [4.0, 0.0, 4.0],
+        ]);
+        input_matrix.transform_to_row_echelon_form();
+        assert!(
+            input_matrix.get_entry(0, 0).expect("No value").is_one()
+                && input_matrix.get_entry(1, 0).expect("No value").is_zero()
+                && input_matrix.get_entry(1, 1).expect("No value").is_one()
+                && input_matrix.get_entry(2, 0).expect("No value").is_zero()
+                && input_matrix.get_entry(2, 1).expect("No value").is_zero()
+                && input_matrix.get_entry(2, 2).expect("No value").is_one()
+                && input_matrix
+                    .get_row(3)
+                    .iter()
+                    .map(|a| a.is_zero())
+                    .reduce(|acc, e| acc && e)
+                    .expect("No value")
+        );
+        Ok(())
+    }
 }
